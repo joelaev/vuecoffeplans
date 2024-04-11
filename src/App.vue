495e36c16b5da2301fd8299f0608014de6944a4d
Joelaev <escobarvj117@gmail.com>
2024-04-11 12:51:26 -0600

    👷🏽‍♂️First property

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,10 @@ import planItem from './components/plan-item.vue';
     </h2>
 
     <div class="plans">
-      <plan-item />
+      <plan-item name="El soltero" gretting-message="hi"/>
+      <plan-item name="El adicto"  gretting-message="bye" />
+      <plan-item name="El viajero" gretting-message="done"/>
+      <plan-item  />
     </div>
   </div>
 </template>
--- src/components/plan-item.vue
+++ src/components/plan-item.vue
@@ -2,12 +2,12 @@
     <div class="plan">
         <div class="description">
           <span class="title">
-            El Soltero
+            {{name}}
           </span>
         </div>
       </div>
 </template>
 
 <script setup>
-
+defineProps(['name']);
 </script>
